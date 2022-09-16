9e4aaa3275603ca8cc7233e7c92ef597bc1086fc
Felipe Rostirolla <felipe.rost.f@gmail.com>
2022-09-16 11:06:25 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <VeeValidateForm/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VeeValidateForm from './components/VeeValidateForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VeeValidateForm
   }
 }
 </script>
